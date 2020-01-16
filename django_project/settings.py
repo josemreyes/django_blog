aa67382ab32a2dfb1c7ee242ca99c9956458f603
unknown <jore5523@gmail.com>
2020-01-16 00:26:51 -0500

    reset smtp for forgot password in setting file

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,7 +133,7 @@ LOGIN_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'gsmtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
